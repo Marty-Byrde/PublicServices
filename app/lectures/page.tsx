45739061ac8368d2c40e1a33af91366291066e85
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-08-31 11:18:09 +0200

    feat: disabled cache of server-side lecture-list page

--- app/lectures/page.tsx
+++ app/lectures/page.tsx
@@ -2,7 +2,8 @@ import ClientPage from "@/app/lectures/ClientPage"
 import { GetLecturesResponse } from "@/app/api/lectures/route"
 
 export default async function Lectures() {
-  const response = await fetch("http://127.0.0.1/api/lectures").then(res => res.json() as Promise<GetLecturesResponse>)
+  const response = await fetch("http://localhost/api/lectures", { cache: "no-cache" }).then(res => res.json() as Promise<GetLecturesResponse>)
+
 
   return (
     <ClientPage initialLectures={response.lectures}/>
