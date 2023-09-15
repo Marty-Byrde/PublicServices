734f1ec6745cb2a7271e93a57ee8036bb2929505
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-09-15 13:30:01 +0200

    feat: added menu-component
    This component provides the functionality that an image, text can be clicked/hovered. When it is hovered or clicked a popover appears underneath it with the given items.

---
+++ app/(components)/Menu/ExpandableMenu.tsx
@@ -0,0 +1,126 @@
+import { Icon, Link, Placement, Popover, PopoverContent, PopoverTrigger } from "@chakra-ui/react"
+import NextImage from "next/image"
+import { ChevronDownIcon, ChevronRightIcon } from "@chakra-ui/icons"
+import * as React from "react"
+import { twMerge } from 'tailwind-merge'
+
+declare type Logical = "start-start" | "start-end" | "end-start" | "end-end" | "start" | "end";
+declare type PlacementWithLogical = Placement | Logical;
+
+export interface ClickableCategoryProps {
+  items: Array<Item>
+  config?: ClickableCategoryConfig
+}
+
+export interface ClickableCategoryConfig {
+  action?: 'click' | 'hover',
+  containerPosition?: PlacementWithLogical,
+  styles?: {
+    popoverBackground?: 'bg-stone-300' | string,
+    popoverBackgroundDark?: 'dark:bg-neutral-700' | string,
+
+    popoverItemHoverBackground?: 'hover:bg-base-200' | string,
+    popoverItemHoverBackgroundDark?: 'dark:hover:bg-gray-900' | string,
+  }
+}
+
+const defaultProps: ClickableCategoryProps = {
+  items: [],
+  config: {
+    action: 'hover',
+    containerPosition: 'bottom-start'
+  },
+}
+
+interface Item {
+  label: string
+  image?: {
+    src: any,
+    imageWidth?: number,
+    imageHeight?: number,
+  }
+  href?: string,
+  subLabel?: string
+  children?: Array<Item>,
+}
+
+/**
+ * This component is used to create a category that can be hovered or clicked for its content/options to appear.
+ * @param args
+ */
+export default function ClickableCategory(args: ClickableCategoryProps) {
+  const props: ClickableCategoryProps = { ...defaultProps, ...args }
+  const { items, config } = props;
+  const { action, containerPosition, styles } = config;
+
+  return (
+    <div className='flex flex-row gap-8'>
+      {items.map((item) => (
+        <div key={item.label} className='group/category z-50'>
+          <Popover trigger={action} placement={containerPosition} offset={[0, 10]}>
+            <PopoverTrigger>
+              <Link className='text-sm font-medium text-gray-600 dark:text-gray-200 hover:no-underline hover:text-gray-800 dark:hover:text-white' href={item.href ?? '#'}>
+                <div className='flex flex-row items-center gap-2'>
+                  {item.image && <NextImage width={item.image.imageWidth ?? 20} height={item.image.imageHeight ?? 20} className='rounded-xl' src={item.image.src} alt='Navitem-Image'/>}
+
+                  {item.label}
+                  <Icon
+                    as={ChevronDownIcon}
+                    className={`group-hover/category:transform group-hover/category:rotate-180 transition-all duration-300 ease-in-out -ml-1`}
+                    w={22}
+                    h={22}
+                  />
+                </div>
+              </Link>
+            </PopoverTrigger>
+
+            {item.children && (
+              <PopoverContent className={twMerge(
+                'bg-stone-300 dark:bg-neutral-700 p-4 rounded-xl min-w-sm border-0 box-shadow-xl',
+                `${styles?.popoverBackground ?? ""} ${styles?.popoverBackgroundDark ?? ""}`
+              )}>
+                <div className='flex flex-col'>
+                  {item.children.map((item) => (
+                    <CategoryCollapseableContent key={item.label} item={item} config={config}/>
+                  ))}
+                </div>
+              </PopoverContent>
+            )}
+
+          </Popover>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+/**
+ * This component is used to create a clickable item that is displayed when a category is clicked/hovered.
+ * @param item - The item that should be displayed when the category is clicked/hovered, inside the newly appeared container
+ */
+function CategoryCollapseableContent({ item, config }: { item: Item, config: ClickableCategoryConfig }) {
+  const { label, subLabel, href } = item;
+
+  return (
+    <Link className={twMerge('group block p-2 px-4 rounded-md hover:bg-base-200 dark:hover:bg-gray-900', `${config?.styles?.popoverItemHoverBackground ?? ""} ${config?.styles?.popoverItemHoverBackgroundDark ?? ""}`)} href={href}>
+      <div className='flex flex-row items-center'>
+        <div className='chakra-box'>
+          <div className='transition-colors duration-200 ease-linear font-medium group-hover:text-secondary group-hover:font-semibold dark:group-hover:text-pink-400'>
+            {label}
+          </div>
+          <div className='text-sm'>
+            {subLabel}
+          </div>
+        </div>
+
+        <div className='flex flex-1 items-center justify-end
+              opacity-50 group-hover:opacity-100
+              group-hover:translate-x-[10px] ml-2
+              transition-all duration-300 ease-linear'>
+          <Icon color={'pink.400'} w={24} h={24} as={ChevronRightIcon}/>
+        </div>
+      </div>
+
+    </Link>
+  )
+}
