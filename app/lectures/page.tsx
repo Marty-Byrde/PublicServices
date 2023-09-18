43908e6a68ade9a235c88750ba7496471d957c26
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-09-18 19:12:34 +0200

    feat: inserted new Lecture-List component to lectures-page
    The LectureList component is now used inside the lectures page.

--- app/lectures/page.tsx
+++ app/lectures/page.tsx
@@ -1,11 +1,15 @@
 import ClientPage from "@/app/lectures/ClientPage"
 import { GetLecturesResponse } from "@/app/api/lectures/route"
+import useSessionData from "@/app/(components)/Auth/useSessionData"
+import { SemesterProps } from "@/app/display/[semester]/SemesterSelection"
+import LectureList from "@/app/lectures/LectureList"
 
 export default async function Lectures() {
+  const { data } = await useSessionData()
   const response = await fetch("http://localhost/api/lectures", { cache: "no-cache" }).then(res => res.json() as Promise<GetLecturesResponse>)
-
+  const semesters: SemesterProps[] = [{ year: 2023, season: 'W' }, { year: 2023, season: 'S' }, { year: 2022, season: 'W' }, { year: 2022, season: 'S' }, { year: 2021, season: 'W' }, { year: 2021, season: 'S' }]
 
   return (
-    <ClientPage initialLectures={response.lectures}/>
+    <LectureList lectures={response.lectures} sessionData={data} semesters={semesters}/>
   )
 }
