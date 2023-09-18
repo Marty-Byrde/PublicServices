5f34a1ffc4e3ddd329ce5e17ce1b632714beb74e
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-09-18 16:59:40 +0200

    feat: modified className composition of Card-component
    The classNames of the card-component are now merged using the tailwind-merge package. Note, that the key-property must be set, for the card-component, when the classNames are to be updated.

--- app/(components)/Cards/Card.tsx
+++ app/(components)/Cards/Card.tsx
@@ -1,3 +1,4 @@
+import { twMerge } from "tailwind-merge"
 interface CardProps {
   children: JSX.Element | JSX.Element[],
   hidden?: boolean,
@@ -12,11 +13,14 @@ interface CardProps {
  * @param className Additional classes
  * @param preventBreakup Prevents the card from breaking up its content into multiple columns
  */
-export default function Card({ children, hidden, className, preventBreakup }: CardProps) {
+export default function Card({ children, hidden, className: _modifiedClasses, preventBreakup }: CardProps) {
   if (hidden) return null;
 
+  const defaultClassnames = ["bg-stone-200", "dark:bg-base-100", "border-[1px]", "border-black", " p-4", " shadow-2xl", `${preventBreakup ? "break-inside-avoid" : ""}`]
+  const _className = twMerge(defaultClassnames.join(" "), _modifiedClasses)
+
   return (
-    <div className={`bg-stone-200 dark:bg-base-100 border-[1px] border-black p-4 shadow-2xl ${preventBreakup ? "break-inside-avoid" : ""} ${className}`}>
+    <div className={_className}>
       {children}
     </div>
   )
