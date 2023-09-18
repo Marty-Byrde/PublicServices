1d29008dd2db7d3485ef28e7226a95866f5a4d32
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-09-18 18:41:31 +0200

    feat: updated semester-selection component
    Removed 'onSelection' property and wrapped the Card inside of a next/Link Tag, which removes the need for the onClick handler of the card-component.

--- app/display/[semester]/SemesterSelection.tsx
+++ app/display/[semester]/SemesterSelection.tsx
@@ -3,6 +3,7 @@ import Card from "@/app/(components)/Cards/Card"
 import { Text } from "@/app/(components)/ResponsiveTags/Text"
 import { Context, createContext, useContext, useEffect, useState } from "react"
 import { updateSemesterSelection } from "@/app/display/[semester]/SemesterActions"
+import Link from "next/link"
 
 const SemesterSelectionContext: Context<any> = createContext(null)
 
@@ -10,14 +11,13 @@ export interface SemesterSelectionProps {
   initialSelection: string | "23W"
   semesters: SemesterProps[],
   showSubtiles?: boolean,
-  onSelection?: () => void
 }
 export interface SemesterProps {
   year: number,
   season: 'W' | 'S'
 }
 
-export default function SemesterSelection({ initialSelection, semesters, showSubtiles, onSelection }: SemesterSelectionProps) {
+export default function SemesterSelection({ initialSelection, semesters, showSubtiles }: SemesterSelectionProps) {
   const [selectedSemester, setSelectedSemester] = useState<string>(initialSelection)
   const [initialRender, setInitialRender] = useState<boolean>(true)
 
@@ -31,7 +31,7 @@ export default function SemesterSelection({ initialSelection, semesters, showSub
   }, [selectedSemester])
 
   return (
-    <SemesterSelectionContext.Provider value={{ selectedSemester, setSelectedSemester, showSubtiles, onSelection }}>
+    <SemesterSelectionContext.Provider value={{ selectedSemester, setSelectedSemester, showSubtiles }}>
       <div className='flex flex-col gap-2'>
         <div className='flex gap-4 flex-wrap mx-auto'>
           {semesters?.map(({ year, season }) => <Semester key={`${year}${season}`} year={year} season={season}/>)}
@@ -42,25 +42,26 @@ export default function SemesterSelection({ initialSelection, semesters, showSub
 }
 
 function Semester({ year, season }: { year: number, season: 'W' | 'S' }) {
-  const { setSelectedSemester, selectedSemester, showSubtiles, onSelection } = useContext(SemesterSelectionContext)
+  const { setSelectedSemester, selectedSemester, showSubtiles } = useContext(SemesterSelectionContext)
   const id = `${year.toString().slice(2)}${season}`
-
 
   const handleClick = () => {
     setSelectedSemester(`${year.toString().slice(2)}${season}`)
-    onSelection?.call()
   }
   const classNames = [`${showSubtiles ?? 'p-2'}`, 'rounded-2xl', 'dark:hover:bg-neutral-700', 'dark:active:bg-neutral-600', 'hover:rounded-2xl', `${selectedSemester === id ? 'dark:bg-red-600 dark:hover:bg-red-500' : ""}`]
 
   return (
-    <Card key={id + (selectedSemester === id)} onClick={handleClick} className={classNames.join(" ")} preventBreakup>
-      <div className='flex flex-col justify-center gap-2'>
-        <Text content={`${year.toString().slice(2)}${season}`} textSize={`${showSubtiles ? 'text-3xl' : 'text-lg'}`} className='text-center'/>
-        <div className={`${showSubtiles ? 'flex' : 'hidden'} gap-1 justify-center`}>
-          <Text content={season === 'W' ? "Winter" : "Sommer"} textSize='text-sm'/>
-          <Text content={year.toString()} textSize='text-sm'/>
+    <Link href={`/lectures/${id}`} onClick={handleClick}>
+      <Card key={id + (selectedSemester === id)} className={classNames.join(" ")} preventBreakup>
+
+        <div className='flex flex-col justify-center gap-2'>
+          <Text content={`${year.toString().slice(2)}${season}`} textSize={`${showSubtiles ? 'text-3xl' : 'text-lg'}`} className='text-center'/>
+          <div className={`${showSubtiles ? 'flex' : 'hidden'} gap-1 justify-center`}>
+            <Text content={season === 'W' ? "Winter" : "Sommer"} textSize='text-sm'/>
+            <Text content={year.toString()} textSize='text-sm'/>
+          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </Link>
   )
 }
