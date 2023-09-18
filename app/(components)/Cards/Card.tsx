5ef752505c126cd96107d74f3014a326049ff4d8
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-09-18 17:00:07 +0200

    feat: added onClick property to card-component

--- app/(components)/Cards/Card.tsx
+++ app/(components)/Cards/Card.tsx
@@ -4,6 +4,7 @@ interface CardProps {
   hidden?: boolean,
   className?: string,
   preventBreakup?: boolean,
+  onClick?: () => void
 }
 
 /**
@@ -13,14 +14,14 @@ interface CardProps {
  * @param className Additional classes
  * @param preventBreakup Prevents the card from breaking up its content into multiple columns
  */
-export default function Card({ children, hidden, className: _modifiedClasses, preventBreakup }: CardProps) {
+export default function Card({ children, hidden, className: _modifiedClasses, preventBreakup, onClick }: CardProps) {
   if (hidden) return null;
 
   const defaultClassnames = ["bg-stone-200", "dark:bg-base-100", "border-[1px]", "border-black", " p-4", " shadow-2xl", `${preventBreakup ? "break-inside-avoid" : ""}`]
   const _className = twMerge(defaultClassnames.join(" "), _modifiedClasses)
 
   return (
-    <div className={_className}>
+    <div onClick={onClick} className={_className}>
       {children}
     </div>
   )
