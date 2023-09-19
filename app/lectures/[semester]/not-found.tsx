6dc19ba1d7ba3241abf27a41fc28131b2ee4fb35
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-09-19 17:35:14 +0200

    feat: added not-found page to [semester] directory
    This page redirects the user to a valid semester based on the user data stored in the database or the default semester-value.

---
+++ app/lectures/[semester]/not-found.tsx
@@ -0,0 +1,8 @@
+import SemesterSelection from "@/components/[semster]/SemesterSelection"
+import useSessionData from "@/components/Auth/useSessionData"
+import { redirect } from "next/navigation"
+
+export default async function SemesterNotFound() {
+  const { data } = await useSessionData()
+  redirect(`/lectures/${data?.lectureStore?.semester ?? process.env.DEFAULT_LECTURES_SEMESTER}`)
+}
