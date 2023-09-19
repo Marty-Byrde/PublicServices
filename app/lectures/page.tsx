0e3cd83eeed9a6332bf49f7403ef00e6ff7b27c3
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-09-19 14:49:49 +0200

    feat: modified the lecture-api-fetch options
    The api request is now a Post request with a body containing the default or previously selected semester.

--- app/lectures/page.tsx
+++ app/lectures/page.tsx
@@ -6,10 +6,17 @@ import LectureList from "@/app/lectures/LectureList"
 
 export default async function Lectures() {
   const { data } = await useSessionData()
-  const response = await fetch("http://localhost/api/lectures", { cache: "no-cache" }).then(res => res.json() as Promise<GetLecturesResponse>)
-  const semesters: SemesterProps[] = [{ year: 2023, season: 'W' }, { year: 2023, season: 'S' }, { year: 2022, season: 'W' }, { year: 2022, season: 'S' }, { year: 2021, season: 'W' }, { year: 2021, season: 'S' }]
+  const response = await fetch(`http://localhost/api/lectures`,
+    {
+      cache: "no-cache",
+      method: 'POST',
+      body: JSON.stringify({
+        semester: data?.lectureStore?.semester ?? process.env.DEFAULT_LECTURES_SEMESTER
+      })
+    }).then(res => res.json() as Promise<GetLecturesResponse>)
+  // const semesters: SemesterProps[] = [{ year: 2023, season: 'W' }, { year: 2023, season: 'S' }, { year: 2022, season: 'W' }, { year: 2022, season: 'S' }, { year: 2021, season: 'W' }, { year: 2021, season: 'S' }]
 
   return (
-    <LectureList lectures={response.lectures} sessionData={data} semesters={semesters}/>
+    <LectureList lectures={response.lectures} sessionData={data} semesters={response?.semesters}/>
   )
 }
