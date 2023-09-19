01845f639ac848f38c3773acef90e9711696b25b
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-09-19 17:36:34 +0200

    feat: added not-found call when the response returns an error
    When the /lectures api-endpoints responds with an not-found error then the user will be shown the not-found page.

--- app/lectures/[semester]/page.tsx
+++ app/lectures/[semester]/page.tsx
@@ -1,21 +1,28 @@
 import useSessionData from "@/components/Auth/useSessionData"
 import { GetLecturesResponse } from "@/api/lectures/route"
 import LectureList from "@/components/[semster]/LectureList"
+import SemesterSelection from "@/components/[semster]/SemesterSelection"
+import LectureSearch from "@/components/[semster]/LectureSearch"
+import LectureListProvider from "@/components/[semster]/LectureListProvider"
+import { notFound } from "next/navigation"
 
 export default async function SemesterLecturePage({ params }) {
   const { semester }: { semester: string } = params
   const { data } = await useSessionData()
+  
   const response = await fetch(`http://localhost/api/lectures`,
     {
       cache: "no-cache",
       method: 'POST',
       body: JSON.stringify({
-        semester: data?.lectureStore?.semester ?? process.env.DEFAULT_LECTURES_SEMESTER
+        semester
       }),
       next:{
         tags: ["lecture-fetch"]
       }
     }).then(res => res.json() as Promise<GetLecturesResponse>)
+
+  if(response?.error?.type === "not-found") return notFound()
 
   return (
     <LectureList lectures={response.lectures} sessionData={data} semester={semester} semesters={response?.semesters}/>
