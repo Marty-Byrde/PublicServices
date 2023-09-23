33b0948f1dfeefd212bc718133b50ac6421240b0
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-09-23 21:02:41 +0200

    feat: externalized Lecture-list rendering
    Externalized the rendering of lectures to an external component to access the filtered lectures from the lecture-provider. This is necessary as the Lecture-container is a server component and the lecture-list is a client component, as it accesses the context of the lecture-provider.

---
+++ app/(components)/[semster]/LectureList.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { LectureListItem } from "@/components/[semster]/LectureListItem"
+import { useContext } from "react"
+import { LectureListContext } from "@/components/[semster]/LectureListProvider"
+import { BasicLecture } from "campus-scraper"
+
+export default function LectureList({ semester, isPending }: { semester: string | '22W', isPending: boolean }) {
+  const { lectures }: { lectures: BasicLecture[] } = useContext(LectureListContext)
+  return (
+    <div className='columns-sm space-y-6 gap-6'>
+      {lectures.map((lecture) => <LectureListItem detailsHref={`/lectures/${semester}/${lecture?.coursePage?.split("/")?.at(-1)?.split(';').at(0)}`} key={lecture.id} lecture={lecture} isPending={isPending}/>)}
+    </div>
+  )
+}
