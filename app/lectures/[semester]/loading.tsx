3373db385aa9fdeee60b7dc23a586c6c97a3781d
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-09-23 21:16:26 +0200

    fix: fixed duplicate key error
    This error occurred as the id of an empty-lecture-list-item was set to '' and this item was set for each element of the Array with the size of 30 elements. Hence, all of the items had the same key. For that reason the id property's value was set to a random number and the array is no longer filled with the same value but instead mapped over, which calls the dummyBasic function for each one.

--- app/lectures/[semester]/loading.tsx
+++ app/lectures/[semester]/loading.tsx
@@ -3,14 +3,14 @@ import { BasicLecture } from "campus-scraper"
 
 export default async function Loading() {
   const dummyBasic = (): BasicLecture => ({
-    id: '',
+    id: Math.random().toString(),
     coursePage: '',
     type: '',
     sws: 0,
     name: '',
     teachers: Array(3).fill(''),
   })
-  const dummyLectures = Array(30).fill(dummyBasic())
+  const dummyLectures = Array(30).map(() => dummyBasic())
 
   return <LectureListContainer isPending lectures={dummyLectures} sessionData={null} semester={null} semesters={null}/>
 }
