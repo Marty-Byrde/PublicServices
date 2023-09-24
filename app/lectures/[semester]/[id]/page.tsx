006a6e7ce0d86597484ca68d204ba5b859d56b6b
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-09-24 17:09:07 +0200

    feat: added dynamic meta title & description
    Added generateMetadata function that sets the meta-description and title for the detailed lecture page based on the selected lecture.

--- app/lectures/[semester]/[id]/page.tsx
+++ app/lectures/[semester]/[id]/page.tsx
@@ -2,8 +2,25 @@ import { Lecture } from "campus-scraper"
 import { Metadata } from "next"
 import LectureDetailsDisplay from "@/app/lectures/[semester]/[id]/LectureDetails"
 
-export const metadata: Metadata = {};
+interface LectureDetailsProps {
+  params: {
+    id: string
+  }
+}
+export async function generateMetadata({ params: { id } }: LectureDetailsProps): Promise<Metadata> {
+  const lecture = await fetch("http://localhost/api/lectureDetails", {
+    method: "POST",
+    body: JSON.stringify({ id: id }),
+    next: {
+      revalidate: 1800
+    }
+  }).then(res => res.json() as Promise<Lecture>)
 
+  return {
+    title: `${lecture.type ? `[${lecture.type}]` : ''} ${lecture.id} - ${lecture.name}`,
+    description: `Detailed information about ${lecture.name}`
+  }
+}
 export default async function LectureDetails({ params }) {
   const { id }: { id: string } = params
   const lecture = await fetch("http://localhost/api/lectureDetails", {
@@ -14,7 +31,6 @@ export default async function LectureDetails({ params }) {
 
 
   if (!lecture) return null
-  metadata.title = `${lecture?.id} - ${lecture?.name}`
 
   return LectureDetailsDisplay({ lecture })
 }
