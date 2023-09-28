d25a83c7a4ed6adb2b95ef274788dbfb326a6f65
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-09-28 09:32:31 +0200

    refactor: updated LectureList props to accept routeParams
    This component now no longer needs the SessionData to determinate the full route of lecture-item, as it gets the study and semester route params passed by its parent. This can be done as the study-selection is validated in the root-page of the lecture-display process (/[semester]/page) and thus the case of an invalid study may not occur.

--- app/(components)/[semster]/LectureList.tsx
+++ app/(components)/[semster]/LectureList.tsx
@@ -5,11 +5,12 @@ import { useContext } from "react"
 import { LectureListContext } from "@/components/[semster]/LectureListProvider"
 import { BasicLecture } from "campus-scraper"
 
-export default function LectureList({ semester, isPending }: { semester: string | '22W', isPending: boolean }) {
+export default function LectureList({ isPending, routeParams: {semester, study} }: { routeParams: {study: string, semester: string}, semester: string | '22W', isPending: boolean }) {
   const { lectures }: { lectures: BasicLecture[] } = useContext(LectureListContext)
+
   return (
     <div className='columns-sm space-y-3 gap-6'>
-      {lectures.map((lecture) => <LectureListItem detailsHref={`/lectures/${semester}/${lecture?.coursePage?.split("/")?.at(-1)?.split(';').at(0)}`} key={lecture.id} lecture={lecture} isPending={isPending}/>)}
+      {lectures.map((lecture) => <LectureListItem detailsHref={`/lectures/${study}/${semester}/${lecture?.coursePage?.split("/")?.at(-1)?.split(';').at(0)}`} key={lecture.id} lecture={lecture} isPending={isPending}/>)}
     </div>
   )
 }
