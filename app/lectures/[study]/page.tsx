9090590c928670d4f60ec27ecaa2fc60ca6b4ab5
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-09-28 09:12:17 +0200

    feat: updated /[study]/page with Link to .../[semester]
    When a study is selected, thus the Link component wrapped around the title of the study is clicked, then the user will be redirected to the initial display of lectures based on the users' preference or default value.

--- app/lectures/[study]/page.tsx
+++ app/lectures/[study]/page.tsx
@@ -1,21 +1,26 @@
 import { GetStudiesResponse } from "@/api/studies/route"
 import { Curricular, StudyPlan } from "@/api/studies/retrieval"
 import Card from "@/components/Cards/Card"
+import Link from "next/link"
+import useSessionData, { SessionData } from "@/components/Auth/useSessionData"
 
 export default async function StudySelection() {
   const { studies } = await fetch("http://localhost/api/studies", {next: {revalidate: 3600 * 24 * 7 * 4.3 * 4}}).then(res => res.json() as Promise<GetStudiesResponse>)
+  const { user, data } = await useSessionData()
+  const subRoute = '/lectures'
+  const semester = user ? data?.lectureStore?.semester ?? process.env.DEFAULT_LECTURES_SEMESTER : process.env.DEFAULT_LECTURES_SEMESTER
 
   return (
     <div>
       <h1>Select your study:</h1>
       <div className='columns-sm gap-6 space-y-3'>
-        {studies.map(plan => plan.curriculars.map((curricular) => (<DisplayStudPlan key={curricular.id} type={plan.type} curricular={curricular}/>)))}
+        {studies.map(plan => plan.curriculars.map((curricular) => (<DisplayStudPlan prefixRoute={subRoute} semester={semester} key={curricular.id} type={plan.type} curricular={curricular}/>)))}
       </div>
     </div>
   )
 }
 
-function DisplayStudPlan({ curricular, type }: { curricular: Curricular, type: string }) {
+function DisplayStudPlan({ curricular, type, prefixRoute, semester }: {prefixRoute: string, semester: string, curricular: Curricular, type: string }) {
   const useStudyPlan = () => {
 
     let backgroundColor = 'bg-blue-400';
@@ -50,7 +55,7 @@ function DisplayStudPlan({ curricular, type }: { curricular: Curricular, type: s
           <div className={`icon px-3 py-1 ${backgroundColor} absolute -left-0 -top-5 rounded-2xl`}>
             {type}
           </div>
-          <div className='text-xl font-semibold line-clamp-2' title={curricular.name}>{curricular.name}</div>
+          <Link href={`${prefixRoute}/${curricular.id}/${semester}`} className='text-xl font-semibold line-clamp-2 hover:text-primary' title={curricular.name}>{curricular.name}</Link>
         </div>
         <div className='flex gap-x-6 flex-wrap'>
           <div className='text-md'>Version: {curricular.details.version}</div>
