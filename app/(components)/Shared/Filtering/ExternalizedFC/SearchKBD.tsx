3ca213b81290055accfa52bad38e9380aa8d2b21
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-10-08 17:33:56 +0200

    feat: externalized KBD of Search-Input field

---
+++ app/(components)/Shared/Filtering/ExternalizedFC/SearchKBD.tsx
@@ -0,0 +1,8 @@
+export default function SearchKBD({ modifier, kbdKey }: { modifier: 'Ctrl' | '⌘', kbdKey: string }) {
+  return (
+    <kbd className="ml-auto flex gap-1 font-medium text-slate-400 dark:text-slate-500">
+      <kbd className="font-sans">{modifier}</kbd>
+      <kbd className="font-sans">{kbdKey.toUpperCase()}</kbd>
+    </kbd>
+  )
+}
