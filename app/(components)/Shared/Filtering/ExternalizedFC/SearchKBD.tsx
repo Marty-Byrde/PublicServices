035c89f2dfed259fa40c9592638a80621c118f43
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-10-08 20:26:29 +0200

    feat: added className property to Search-KBD component

--- app/(components)/Shared/Filtering/ExternalizedFC/SearchKBD.tsx
+++ app/(components)/Shared/Filtering/ExternalizedFC/SearchKBD.tsx
@@ -1,8 +1,10 @@
-export default function SearchKBD({ modifier, kbdKey }: { modifier: 'Ctrl' | '⌘', kbdKey: string }) {
+import { twMerge } from "tailwind-merge"
+
+export default function SearchKBD({ modifier, kbdKey, className }: { modifier: 'Ctrl' | '⌘', kbdKey: string, className?: string }) {
   if(!modifier) return null;
 
   return (
-    <kbd className="ml-auto flex gap-1 font-medium text-slate-400 dark:text-slate-500">
+    <kbd key={className} className={twMerge('ml-auto flex gap-1 font-medium text-slate-400 dark:text-slate-500', className)}>
       <kbd className="font-sans">{modifier}</kbd>
       <kbd className="font-sans">{kbdKey.toUpperCase()}</kbd>
     </kbd>
