e4af7fe545e75bf30bf1146e433001b83dfbd28a
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-10-09 12:20:49 +0200

    refactor: replaced Lecture-Provider with FilteringProvider

--- app/(components)/[semster]/LectureList.tsx
+++ app/(components)/[semster]/LectureList.tsx
@@ -4,13 +4,14 @@ import { LectureListItem } from "@/components/[semster]/LectureListItem"
 import { useContext } from "react"
 import { LectureListContext } from "@/components/[semster]/LectureListProvider"
 import { BasicLecture } from "campus-scraper"
+import { FilterProviderContext, FilterProviderContextProps } from "@/components/Shared/Filtering/FilteringProvider"
 
 export default function LectureList({ isPending, routeParams: {semester, study} }: { routeParams: {study: string, semester: string}, semester: string | '22W', isPending: boolean }) {
-  const { lectures }: { lectures: BasicLecture[] } = useContext(LectureListContext)
+  const { filter }: FilterProviderContextProps<BasicLecture> = useContext(FilterProviderContext)
 
   return (
     <div className='columns-sm space-y-3 gap-6'>
-      {lectures.map((lecture) => <LectureListItem detailsHref={`/lectures/${study}/${semester}/${lecture?.coursePage?.split("/")?.at(-1)?.split(';').at(0)}`} key={lecture.id} lecture={lecture} isPending={isPending}/>)}
+      {filter.map((lecture) => <LectureListItem detailsHref={`/lectures/${study}/${semester}/${lecture?.coursePage?.split("/")?.at(-1)?.split(';').at(0)}`} key={lecture.id} lecture={lecture} isPending={isPending}/>)}
     </div>
   )
 }
