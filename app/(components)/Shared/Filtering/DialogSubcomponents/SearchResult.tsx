711fb8947530b9d5a48283f191f590a68a798676
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-10-09 12:30:24 +0200

    fix: fixed getItemProps arguments
    Previously hovering over an search-result in the dialog highlighted all the other results as well, this was due to the missing properties that are present in the BaseItem. As a result the 'result' variable is typecasted as Baseitem.

--- app/(components)/Shared/Filtering/DialogSubcomponents/SearchResult.tsx
+++ app/(components)/Shared/Filtering/DialogSubcomponents/SearchResult.tsx
@@ -1,24 +1,26 @@
 import { Fragment, useId } from "react"
 import HighlightQuery from "@/components/Shared/Filtering/DialogSubcomponents/HighlightQuery"
 import { AutoComplete_ResultItem, useAutocompleteResultProps } from "@/components/Shared/Filtering/DialogSubcomponents/hooks/useAutoComplete"
+import { BaseItem } from "@algolia/autocomplete-core"
 
 export default function SearchResult({ result, autocomplete, collection, query }: { result: AutoComplete_ResultItem, autocomplete: useAutocompleteResultProps['autocomplete'], collection: any, query: string }) {
   let id = useId()
 
-  return (
-    <li className="group block cursor-default rounded-lg px-3 py-2 aria-selected:bg-slate-100 dark:aria-selected:bg-slate-700/30"
-        aria-labelledby={`${id}-hierarchy ${id}-title`}
-        {...autocomplete.getItemProps({ item: result.baseItem, source: collection.source, })}>
+  // @ts-ignore
+  return (<li {...autocomplete.getItemProps({ item: (result as BaseItem), source: collection.source })}
+              className="group block cursor-default rounded-lg px-3 py-2 aria-selected:bg-slate-100 dark:aria-selected:bg-slate-700/30"
+              aria-labelledby={`${Math.random()}-hierarchy ${result.title}-title`}
+    >
 
-      <div id={`${id}-title`} aria-hidden="true"
+      <div id={`${result.title}-title`} aria-hidden="true"
            className="text-sm text-slate-700 group-aria-selected:text-sky-600 dark:text-slate-300 dark:group-aria-selected:text-sky-400">
         <HighlightQuery text={result.title} query={query}/>
       </div>
 
-      <div id={`${id}-hierarchy`} aria-hidden="true"
+      <div id={`${result.title}-hierarchy`} aria-hidden="true"
            className="mt-0.5 truncate whitespace-nowrap text-xs text-slate-500 dark:text-slate-400">
 
-        <Fragment key={'SearchResultFragment' + id}>
+        <Fragment key={'SearchResultFragment' + id + result.title}>
           {result?.description && <HighlightQuery text={result?.description} query={query}/>}
           <span className={!result?.route ? 'sr-only' : 'mx-2 text-slate-300 dark:text-slate-700'}>
             {result?.route}
