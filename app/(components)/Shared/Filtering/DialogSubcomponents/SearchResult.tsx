44400395b2fb6eb327b7fb1f86d34b4adefc50b0
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-10-11 12:55:14 +0200

    fix: updated keys for SearchResults and Result

--- app/(components)/Shared/Filtering/DialogSubcomponents/SearchResult.tsx
+++ app/(components)/Shared/Filtering/DialogSubcomponents/SearchResult.tsx
@@ -9,15 +9,15 @@ export default function SearchResult({ result, autocomplete, collection, query }
   // @ts-ignore
   return (<li {...autocomplete.getItemProps({ item: (result as BaseItem), source: collection.source })}
               className="group block cursor-default rounded-lg px-3 py-2 aria-selected:bg-slate-100 dark:aria-selected:bg-slate-700/30"
-              aria-labelledby={`${Math.random()}-hierarchy ${result.title}-title`}
+              aria-labelledby={`${Math.random()}-hierarchy ${result.title}-title--${id}`}
     >
 
-      <div id={`${result.title}-title`} aria-hidden="true"
+      <div id={`${result.title}-title-${id}`} aria-hidden="true"
            className="text-sm text-slate-700 group-aria-selected:text-sky-600 dark:text-slate-300 dark:group-aria-selected:text-sky-400">
         <HighlightQuery text={result.title} query={query}/>
       </div>
 
-      <div id={`${result.title}-hierarchy`} aria-hidden="true"
+      <div id={`${result.title}-hierarchy-${id}`} aria-hidden="true"
            className="mt-0.5 truncate whitespace-nowrap text-xs text-slate-500 dark:text-slate-400">
 
         <Fragment key={'SearchResultFragment' + id + result.title}>
