fa57c7782980c6f47c132f3a76e095a4758a0980
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-10-23 17:58:22 +0200

    feat: added QuizQuestion component
    This component is used to display a question of Kahoot quiz.

---
+++ app/(components)/kahoot/QuizQuestion.tsx
@@ -0,0 +1,24 @@
+import { QuizQuestion } from "@/typings/kahoot/kahoot"
+import { Text } from "@/components/ResponsiveTags/Text"
+import QuizAnswer from "@/components/kahoot/QuizAnswer"
+
+interface QuizQuestionProps {
+  question: QuizQuestion
+  isPending: boolean
+  index: number
+}
+
+export default function QuizQuestion({ question: { question, answers }, isPending, index }: QuizQuestionProps) {
+  return (
+    <div className='relative px-6 py-4 ring-2 dark:ring-neutral-500 rounded-2xl dark:bg-neutral-700/40 flex flex-col gap-6 break-inside-avoid-column'>
+      <div className='absolute top-2.5 left-2.5 px-3 py-1 pb-1.5 bg-orange-800 rounded-full text-gray-300 font-bold text-xl'>{index}</div>
+      <Text content={question} isPending={isPending} textSize='text-lg' className='ml-8 font-bold tracking-wide' darkColor='text-gray-300'/>
+
+      <ul className='flex flex-col gap-6'>
+        {answers.map((answer, i) => (
+          <QuizAnswer key={`${answer.text}-${i}`} answer={answer} index={i} isPending={isPending}/>
+        ))}
+      </ul>
+    </div>
+  )
+}
