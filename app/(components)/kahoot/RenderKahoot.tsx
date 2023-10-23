1cb71b58bd0fd3b020c940c145fcdc2d9cdf3a36
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-10-23 18:02:14 +0200

    feat: created RenderKahootQuiz component
    This component fetches a quiz from a given endpoint and then renders the quiz.

---
+++ app/(components)/kahoot/RenderKahoot.tsx
@@ -0,0 +1,57 @@
+import { Quiz, QuizAnswer, QuizQuestion } from "@/typings/kahoot/kahoot"
+import KahootQuiz from "@/components/kahoot/KahootQuiz"
+
+interface RenderKahootProps {
+  quizzId: string
+}
+
+export default async function RenderKahoot({ quizzId }: RenderKahootProps) {
+  const quiz = await getQuiz(quizzId)
+
+  return (
+    <div>
+      {/*// @ts-ignore*/}
+      <KahootQuiz quiz={quiz} isPending={false}/>
+    </div>
+  )
+}
+
+interface KahootAPIResponse {
+  success: boolean,
+  answers: KahootAPIAnswer[],
+}
+interface KahootAPIAnswer {
+  question: string,
+  options: KahootAPIOption[],
+  answer: number[],
+}
+interface KahootAPIOption {
+  text: string,
+}
+
+async function getQuiz(quizzId: string): Promise<Quiz> {
+  const response = await fetch(process.env.KAHOOT_ANSWERS_ENDPOINT.replace("%s", quizzId), {
+    headers: {
+      'Cookie': process.env.KAHOOT_ANSWERS_COOKIES
+    },
+    next: {
+      revalidate: 30
+    }
+  }).then(res => res.json())
+  const questions: QuizQuestion[] = response.answers.map((originalQuestion: KahootAPIResponse['answers'][0]) => {
+    const answers: QuizAnswer[] = originalQuestion.options.map((option: KahootAPIResponse['answers'][0]['options'][0], i): QuizAnswer => ({
+      text: option.text,
+      isCorrect: originalQuestion.answer.includes(i),
+    }))
+
+    return {
+      question: originalQuestion.question,
+      answers,
+    }
+  })
+
+  return {
+    quizzId,
+    questions
+  }
+}
