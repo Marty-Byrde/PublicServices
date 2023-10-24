b9b49f096a3725374fd19e38d508e2db09e4cd53
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-10-24 09:15:01 +0200

    refactor: wrapped quiz heading on mobile screens

--- app/(components)/kahoot/KahootQuiz.tsx
+++ app/(components)/kahoot/KahootQuiz.tsx
@@ -10,9 +10,9 @@ interface KahootAnswersProps {
 export default async function KahootQuiz({ quiz: { questions, quizzId }, isPending }: KahootAnswersProps) {
   return (
     <div className='px-4 pb-8'>
-      <div className='flex gap-4 justify-center my-4 mb-8'>
+      <div className='flex gap-4 justify-center my-4 mb-8 flex-col md:flex-row items-center'>
         <Text content='Answers for: ' isPending={isPending} textSize='text-2xl' darkColor='text-gray-200'/>
-        <Text content={quizzId.toString()} isPending={isPending} textSize='text-2xl' darkColor='text-secondary-content'/>
+        <Text content={quizzId.toString()} isPending={isPending} textSize='text-2xl' darkColor='text-secondary-content' className='text-center'/>
       </div>
 
       <div className='grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 3xl:grid-cols-4  gap-10 '>
