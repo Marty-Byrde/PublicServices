10cd5df7510e98d217a07d5e03c89a48e7307da9
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-10-25 17:21:00 +0200

    refactor: renamed KahootQuiz loader & added quizId property

--- app/kahoot/[quizId]/loadingKahootQuiz.tsx
+++ app/kahoot/[quizId]/loadingKahootQuiz.tsx
@@ -1,10 +1,10 @@
 import { Quiz, QuizAnswer, QuizQuestion } from "@/typings/kahoot/kahoot"
 import KahootQuiz from "@/components/kahoot/KahootQuiz"
 
-export default function LoadingKahootQuiz(): JSX.Element {
+export default function LoadingKahootQuiz({quizId}: {quizId: string}): JSX.Element {
   const createDummyQuiz = (): Quiz => ({
-    quizzId: Math.random().toString(),
-    questions: Array.from({ length: 10 }, (_, i) : QuizQuestion => ({
+    quizzId: quizId,
+    questions: Array.from({ length: 8 }, (_, i) : QuizQuestion => ({
       question: Math.random().toString(),
       answers: Array.from({ length: 4 }, (_, i): QuizAnswer => ({
         text: Math.random().toString(),
