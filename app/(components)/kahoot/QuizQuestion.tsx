717763b562d402bc3d37b1a3b5eed84bcbf0f3dd
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-10-25 17:21:58 +0200

    feat: modified skeleton-classname QuizQuestion

--- app/(components)/kahoot/QuizQuestion.tsx
+++ app/(components)/kahoot/QuizQuestion.tsx
@@ -12,7 +12,7 @@ export default function QuizQuestion({ question: { question, answers }, isPendin
   return (
     <div className='relative px-6 py-4 ring-2 ring-neutral-500 dark:ring-neutral-500 rounded-2xl dark:bg-neutral-700/40 flex flex-col gap-6 break-inside-avoid-column'>
       <div className='absolute top-2.5 left-2.5 px-3 py-1 pb-1.5 bg-orange-800 rounded-full text-gray-100 dark:text-gray-300 font-bold text-xl'>{index}</div>
-      <Text content={question} isPending={isPending} textSize='text-lg' className='ml-8 font-bold tracking-wide' darkColor='dark:text-gray-300' color='text-gray-600'/>
+      <Text content={question} isPending={isPending} textSize='text-lg' className='ml-8 font-bold tracking-wide' darkColor='dark:text-gray-300' color='text-gray-600' skeletonClassName='ml-8 mt-2' skHeight='h-3' skWidth='w-[90%]' skDarkBackground='dark:bg-gray-400'/>
 
       <ul className='flex flex-col gap-6'>
         {answers.map((answer, i) => (
