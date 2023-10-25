c90ce2bbb3b9e6da5c69454adc4631b3c1588ddf
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-10-25 17:26:53 +0200

    feat: modified question-text margin for question count. > 9

--- app/(components)/kahoot/QuizQuestion.tsx
+++ app/(components)/kahoot/QuizQuestion.tsx
@@ -9,10 +9,12 @@ interface QuizQuestionProps {
 }
 
 export default function QuizQuestion({ question: { question, answers }, isPending, index }: QuizQuestionProps) {
+  const questionTextMargin = index > 9 ? 'ml-12' : 'ml-8'
+
   return (
     <div className='relative px-6 py-4 ring-2 ring-neutral-500 dark:ring-neutral-500 rounded-2xl dark:bg-neutral-700/40 flex flex-col gap-6 break-inside-avoid-column'>
       <div className='absolute top-2.5 left-2.5 px-3 py-1 pb-1.5 bg-orange-800 rounded-full text-gray-100 dark:text-gray-300 font-bold text-xl'>{index}</div>
-      <Text content={question} isPending={isPending} textSize='text-lg' className='ml-8 font-bold tracking-wide' darkColor='dark:text-gray-300' color='text-gray-600' skeletonClassName='ml-8 mt-2' skHeight='h-3' skWidth='w-[90%]' skDarkBackground='dark:bg-gray-400'/>
+      <Text content={question} isPending={isPending} textSize='text-lg' className={`${questionTextMargin} font-bold tracking-wide`} darkColor='dark:text-gray-300' color='text-gray-600' skeletonClassName={`${questionTextMargin} mt-2`} skHeight='h-3' skWidth='w-[90%]' skDarkBackground='dark:bg-gray-400'/>
 
       <ul className='flex flex-col gap-6'>
         {answers.map((answer, i) => (
