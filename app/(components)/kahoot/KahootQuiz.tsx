23d755da0ce9d9dd578e845b5db7db8132db6877
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-10-25 17:02:02 +0200

    style: updated KahottQuiz styles for light-mode

--- app/(components)/kahoot/KahootQuiz.tsx
+++ app/(components)/kahoot/KahootQuiz.tsx
@@ -11,8 +11,8 @@ export default function KahootQuiz({ quiz: { questions, quizzId }, isPending }: 
   return (
     <div className='px-4 pb-8'>
       <div className='flex gap-4 justify-center my-4 mb-8 flex-col md:flex-row items-center'>
-        <Text content='Answers for: ' isPending={isPending} textSize='text-2xl' darkColor='text-gray-200'/>
-        <Text content={quizzId.toString()} isPending={isPending} textSize='text-2xl' darkColor='text-secondary-content' className='text-center'/>
+        <Text content='Answers for: ' isPending={isPending} textSize='text-2xl' darkColor='dark:text-gray-200' color='text-gray-700'/>
+        <Text content={quizzId.toString()} isPending={isPending} textSize='text-2xl' darkColor='dark:text-secondary-content' color='text-primary' className='text-center'/>
       </div>
 
       <div className='grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 3xl:grid-cols-4  gap-10 '>
