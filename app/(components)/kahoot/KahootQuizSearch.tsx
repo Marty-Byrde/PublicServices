68e1bd0894e92e1f70174687e6fda41bc0c2a744
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-11-11 16:22:44 +0100

    feat: implemented validity functionality to quiz-search-component
    In case the entered quizId is not valid the user will be warned using a toast. However, if the user wants to continue the warning will not be displayed on the second attempt.

--- app/(components)/kahoot/KahootQuizSearch.tsx
+++ app/(components)/kahoot/KahootQuizSearch.tsx
@@ -1,22 +1,40 @@
 'use client'
-import { FormEvent, useContext } from "react"
+import { ChangeEvent, FormEvent, useContext, useState } from "react"
 import { useRouter } from "next/navigation"
 import { KahootSearchContext } from "@/components/kahoot/KahootSearchProvider"
+import checkValidity from "@/lib/kahoot/ValidityCheck"
+import { toast } from "react-toastify"
 
 export default function KahootQuizSearch() {
   const { searchQuery, setSearchQuery }: { searchQuery: string, setSearchQuery: (query: string) => void } = useContext(KahootSearchContext)
+  const [warned, setWarned] = useState<boolean>(false)
+
   const router = useRouter()
 
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const validity = checkValidity({ quizId: searchQuery })
+    if (!warned && validity.valid === false) {
+      setWarned(true)
+      return toast(validity.reason, {
+        type: "warning",
+        autoClose: 5000,
+        closeButton: true,
+      })
+    }
+
     router.push(`kahoot/${searchQuery}`)
   }
 
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value)
+    setWarned(false)
+  }
 
   return (
     <form onSubmit={onSubmit} className='max-w-md w-full'>
       <input defaultValue={searchQuery} type='text'
-             onChange={(e) => setSearchQuery(e.target.value)}
+             onChange={onChange}
              placeholder='d1851d1a-3376-42ce-bc3a-b344a112f971'
              className='px-4 py-2 dark:bg-neutral-700 ring-2 dark:ring-neutral-500 rounded-lg max-w-md w-full text-gray-700 dark:text-gray-200'/>
     </form>
