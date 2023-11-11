3b5931221003687982345020ab149dcda327ef90
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-11-11 16:21:39 +0100

    feat: added checkValidity function
    This function checks whether a given quizId is valid and returns a corresponding reason if it is not.

---
+++ lib/kahoot/ValidityCheck.ts
@@ -0,0 +1,28 @@
+const length = 37
+const sectionLenghts = [8, 4, 4, 4, 12]
+
+interface ValidQuizId {
+  valid: true,
+}
+
+interface InValidQuizzId {
+  valid: false,
+  reason: 'Missing ID Section (-)' |
+    'Section is incomplete (?)' | string
+}
+
+
+export default function checkValidity({ quizId }: { quizId: string }): ValidQuizId | InValidQuizzId {
+
+  // if(quizId.length !== length) return false;
+  if (quizId.split("-").length !== sectionLenghts.length) return { valid: false, reason: "Missing ID Section (-)" };
+  if (quizId.split("-").filter((section: string, index: number) => sectionLenghts[index] !== section.length)) return {
+    valid: false,
+    reason: `Section is incomplete (${quizId.split('-').findIndex((section, index) => sectionLenghts[index] !== section.length)})`
+  };
+
+
+  return {
+    valid: true
+  };
+}
