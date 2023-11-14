97ede668b11c96bb447e45a0bda17c0df97374e8
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-11-14 16:18:21 +0100

    refactor: changed quiz-validity-check string response

--- lib/kahoot/ValidityCheck.ts
+++ lib/kahoot/ValidityCheck.ts
@@ -15,7 +15,7 @@ interface InValidQuizzId {
 export default function checkValidity({ quizId }: { quizId: string }): ValidQuizId | InValidQuizzId {
 
   // if(quizId.length !== length) return false;
-  if (quizId.split("-").length !== sectionLenghts.length) return { valid: false, reason: "Missing ID Section (-)" };
+  if (quizId.split("-").length !== sectionLenghts.length) return { valid: false, reason: "Missing Sections (-)" };
   if (quizId.split("-").filter((section: string, index: number) => sectionLenghts[index] !== section.length).length > 0) return {
     valid: false,
     reason: `Section is incomplete (${quizId.split('-').findIndex((section, index) => sectionLenghts[index] !== section.length)})`
