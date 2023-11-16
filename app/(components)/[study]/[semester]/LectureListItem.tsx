da0cef6ecd310fd44a985b327e7591067846500d
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-11-16 17:41:21 +0100

    refactor: replaced user image with speaker-svg icon

--- app/(components)/[study]/[semester]/LectureListItem.tsx
+++ app/(components)/[study]/[semester]/LectureListItem.tsx
@@ -3,6 +3,7 @@ import Link from "next/link"
 import { Text } from "@/components/Shared/Responsive/Text"
 import ServerImage from "@/components/Shared/Responsive/ServerImage"
 import User from "@/public/lectureIcons/user.svg"
+import SpeakerIcon from "@/components/Shared/Icons/SpeakerIcon"
 
 
 interface LectureListItemProps {
@@ -34,7 +35,7 @@ export function LectureListItem({ lecture, isPending, detailsHref }: LectureList
           <div className={`text-gray-600 dark:text-gray-400 font-semibold flex flex-col gap-x-4 gap-y-2 `}>
             {lecture?.teachers?.map(teacher => (
               <div key={teacher} className='flex flex-row flex-nowrap items-center gap-2'>
-                <ServerImage src={User}
+                <ServerImage src={SpeakerIcon({className: 'w-5 h-5 stroke-sky-700 dark:stroke-sky-600'})}
                              alt='user'
                              width={14}
                              isPending={false}
